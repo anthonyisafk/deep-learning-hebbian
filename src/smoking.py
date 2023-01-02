da1d4105f54381ab25b0433e1d71990b4117b104
anthonyisafk <72876359+anthonyisafk@users.noreply.github.com>
2023-01-02 15:22:49 +0200

    Widen the test size for `smoking`

--- src/smoking.py
+++ src/smoking.py
@@ -3,18 +3,17 @@ import pandas as pd
 from model import *
 from utils.preprocessing import *
 
-eta = 1e-6
+eta = 0.0001
 ncomps = 10
 
 if __name__ == '__main__':
     filename = "smoking/smoking.csv"
     df = pd.read_csv(filename, delimiter=',', header=0)
-    df = df.head(1000)
+    df = df.head(5000)
     nrows, ncols = np.shape(df)
     # print(df)
 
-    df, X = get_features(df, 'smoking', ['gender', 'oral', 'tartar'])
-    X = np.asarray(df.drop(columns=['smoking']))
+    df, X = get_features(df, 'smoking', ['gender', 'oral', 'tartar'], ['ID'])
 
-    hebb = Model([ncols-1, ncomps], eta)
+    hebb = Model([ncols-2, ncomps], eta)
     hebb.train(X)
