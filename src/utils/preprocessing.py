b27698352fcca397ced5e5e36e5d2e3ead2cbe13
anthonyisafk <72876359+anthonyisafk@users.noreply.github.com>
2023-01-02 15:23:40 +0200

    Add a list of columns to drop to `get_features`

--- src/utils/preprocessing.py
+++ src/utils/preprocessing.py
@@ -7,10 +7,13 @@ def get_features(
     df,
     class_col: str,
     encode: List[str] = None,
+    drop: List[str] = None,
 ):
     if encode is not None:
         for e in encode:
             df[e] = LabelEncoder().fit_transform(df[e])
+    if drop is not None:
+        df = df.drop(columns=drop)
     x = np.asarray(df.drop(columns=[class_col]))
-    # x = normalize(x)
+    x = normalize(x)
     return df, x
