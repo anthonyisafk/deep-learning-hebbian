f8f4be70481452faf5e860156273591e434df464
anthonyisafk <72876359+anthonyisafk@users.noreply.github.com>
2023-01-02 20:02:23 +0200

    Add training completion criterion in `Model.train`

--- src/model.py
+++ src/model.py
@@ -1,8 +1,8 @@
 from typing import List
 from layer import *
 
-
-ind = -1
+MAX_EPOCHS = 100
+TOLER = 5e-5
 
 class Model:
     nlayers: int      # number of layers
@@ -20,26 +20,32 @@ class Model:
         self.eta = eta
         self.layers = np.ndarray(dtype=Layer, shape=(2))
         self.layers[0] = Layer('i', n_nodes[0], 1, n_nodes[1])
-        self.layers[1] = Layer('o', n_nodes[1], n_nodes[0], 1, mu=0, stddev=0.1/n_nodes[1])
+        self.layers[1] = Layer('o', n_nodes[1], n_nodes[0], 1, mu=0, stddev=1e-10)
 
 
     def train(self, xs):
         nsamples = len(xs)
-        il = self.layers[0]
         ol = self.layers[1]
         epoch = 0
-        while epoch < 10:
-            xsshuf = np.random.permutation(xs)
+        fit = False
+        while epoch < MAX_EPOCHS and not fit:
             print(f"\n ** Epoch : {epoch}\n")
+            dws = np.zeros(shape=(self.no, self.ni))
+            xsshuf = np.random.permutation(xs)
             for p in range(nsamples):
                 x = xsshuf[p]
                 self.set_ys(x)
                 ol.set_d()
                 for j in range(self.no):
-                    ol.dw[j] = self.eta * ol.y[j] * (x - ol.d[j, :])
-                    ol.w[j] += ol.dw[j]
+                    # ol.d[j, :] = np.dot(ol.w[:j, :].T, ol.y[:j])
+                    ol.dw[j, :] = self.eta * ol.y[j] * (x - ol.d[j, :])
+                    ol.w[j, :] += ol.dw[j, :]
+                dws += ol.dw
             epoch += 1
-            print(ol.w[1])
+            mean_dw = np.mean(np.abs(dws)) / self.eta / nsamples
+            fit = (epoch > self.no) and (mean_dw <= TOLER)
+            print(f" * mean : {mean_dw}")
+            # print(ol.w[1])
 
     def set_ys(self, x):
         self.layers[0].y = self.layers[0].get_y(x)
