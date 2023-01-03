fa1a53b4e22bc9871816e68b09305c91b7a95374
anthonyisafk <72876359+anthonyisafk@users.noreply.github.com>
2023-01-03 03:51:37 +0200

    Create constants.py

---
+++ src/constants.py
@@ -0,0 +1,11 @@
+'''
+ ** Project-wide set of constants.
+'''
+
+# Hebbian learning model fitting.
+MAX_EPOCHS = 100
+TOLERANCE_PER_OUT_NODE = 1e-7
+
+# `smoking` dataset example.
+eta = 0.001
+ncomps = 5
