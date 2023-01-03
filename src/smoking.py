d32d2d1c9dc6896021c62d289758be25f2bbc9b7
anthonyisafk <72876359+anthonyisafk@users.noreply.github.com>
2023-01-03 03:55:21 +0200

    Add project introduction. Calculate covariance and total ratio of variance explained

--- src/smoking.py
+++ src/smoking.py
@@ -1,19 +1,34 @@
-import pandas as pd
+"""
+@brief: PCA implemented with Hebbian Learning.
+        Testing on the `body signals of smoking` dataset:
+@instructions: You can change the number of components
+               and other parameters in the `constants.py` file.
+****************************************
+https://www.kaggle.com/datasets/kukuroo3/body-signal-of-smoking
+****************************************
+@author: Antoniou, Antonios - 9482
+@email: aantonii@ece.auth.gr
+****************************************
+School of Informatics
+2023 Aristotle University Thessaloniki
+"""
 
+import pandas as pd
 from model import *
 from utils.preprocessing import *
-
-eta = 0.0001
-ncomps = 10
+from constants import eta, ncomps
 
 if __name__ == '__main__':
     filename = "smoking/smoking.csv"
     df = pd.read_csv(filename, delimiter=',', header=0)
-    df = df.head(5000)
     nrows, ncols = np.shape(df)
-    # print(df)
 
     df, X = get_features(df, 'smoking', ['gender', 'oral', 'tartar'], ['ID'])
-
+    org_var = np.sum(np.diag(np.cov(X, rowvar=False)))
     hebb = Model([ncols-2, ncomps], eta)
-    hebb.train(X)
+    hebb.fit(X)
+
+    C = np.cov(hebb.comps, rowvar=False)
+    print(C, end="\n\n")
+    C = C / org_var
+    print(np.diag(C))
