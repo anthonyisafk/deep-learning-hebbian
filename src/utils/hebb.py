1e0e0e0fa384189e223838d71e7ffd3c9ba1f0e2
anthonyisafk <72876359+anthonyisafk@users.noreply.github.com>
2023-01-07 23:35:55 +0200

    Implement get_explained_variance()

--- src/utils/hebb.py
+++ src/utils/hebb.py
@@ -1,10 +1,9 @@
 import numpy as np
-
-max_layers = 2
+from constants import MAX_LAYERS
 
 def check_nlayers(n):
-    if n == 1 or n > 2:
-        msg = f"Number of layers given : {n}. Max number of layers supported : {max_layers}"
+    if n == 1 or n > MAX_LAYERS:
+        msg = f"Number of layers given : {n}. Max number of layers supported : {MAX_LAYERS}"
         raise Exception(msg)
 
 
@@ -12,3 +11,13 @@ def check_input_output_nodes(ni, no):
     if no >= ni:
         msg = f"Number of output nodes should be less than {ni}. `{no}` was given."
         raise Exception(msg)
+
+
+def get_explained_variance(comps, X):
+    org_var = np.sum(np.diag(np.cov(X, rowvar=False)))
+    C = np.cov(comps, rowvar=False)
+    C = C / org_var
+    if (comps.shape[1] == 1):
+        return C
+    Cdiag = np.diag(C)
+    return np.sum(Cdiag)
